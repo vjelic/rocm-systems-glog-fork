460659f5427c88e564fa93f570420502960aadd4
Aleksei Tumakaev <atumakae@amd.com>
2025-07-30 14:55:49 +0000

    Write hsa api to CSV

--- projects/rocprofiler-sdk/source/lib/python/rocpd/csv.py
+++ projects/rocprofiler-sdk/source/lib/python/rocpd/csv.py
@@ -260,6 +260,35 @@ def write_hip_api_csv(
     """
     write_sql_query_to_csv(importData, query, output_path, "out_hip_api_trace")
 
+def write_hsa_api_csv(
+    importData, output_path
+) -> None:
+
+    query = """
+        SELECT
+            R.guid AS Guid,
+            R.category AS Domain,
+            R.name AS Function,
+            R.pid AS Process_Id,
+            R.tid AS Thread_Id,
+            R.stack_id AS Correlation_Id,
+            R.start AS Start_Timestamp,
+            R.end AS End_Timestamp
+        FROM "rocpd_info_node" AS N
+        INNER JOIN rocpd_info_process as P
+            ON P.guid = N.guid
+            AND P.nid = N.id
+        INNER JOIN regions AS R
+            ON R.guid = P.guid
+            AND R.nid = P.nid
+            AND R.pid = P.pid
+        WHERE
+            R.category LIKE 'HSA_%'
+        ORDER BY
+            R.start ASC, R.end DESC
+    """
+    write_sql_query_to_csv(importData, query, output_path, "out_hsa_api_trace")
+
 def write_csv(importData, config):
 
     write_agent_info_csv(importData, config.output_path)
@@ -267,6 +296,7 @@ def write_csv(importData, config):
     write_memory_copy_csv(importData, config.output_path)
     write_memory_allocation_csv(importData, config.output_path)
     write_hip_api_csv(importData, config.output_path)
+    write_hsa_api_csv(importData, config.output_path)
 
 def execute(input, config=None, window_args=None, **kwargs):
 
